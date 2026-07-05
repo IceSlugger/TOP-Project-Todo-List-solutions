c463429ce4508894a7f3675a04eef307f33de43f
IceSlugger <adrian8988@gmail.com>
2026-07-06 00:40:51 +0800

    Update webpack output to docs

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -1,11 +1,20 @@
+const path = require('path');
+
 module.exports = {
-  // ... your other config settings (entry, output, etc)
+  mode: 'development', 
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'docs'), // This forces Webpack to use the 'docs' folder
+    clean: true, // This automatically deletes old files in 'docs' before a new build
+  },
   module: {
     rules: [
       {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
+      // You can add your image rule back here if you need it later
     ],
   },
 };
